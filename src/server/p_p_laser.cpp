6eff77faa6b8dd58cc7dacfffa659cd4b566e20a
axel-zielonka <azielonka@fi.uba.ar>
2024-11-24 18:23:37 -0300

    PP laser dispara bien

--- src/server/p_p_laser.cpp
+++ src/server/p_p_laser.cpp
@@ -8,27 +8,31 @@ PewPewLaser::PewPewLaser(posicion_t posicion_inicial):
         Arma(ID_PP_LASER, PEW_PEW_LASER, ConfigJuego::ALCANCE_MINIMO_PP_LASER, ConfigJuego::MUNICIONES_PP_LASER, false, posicion_inicial),
         alcance_maximo(ConfigJuego::ALCANCE_MAXIMO_PP_LASER) {}
 
-inclinacion_e buscar_inclinacion(const int& bala){
+
+
+inclinacion_e obtener_inclinacion(const int& bala){
     if (bala == 0) return NO_TIENE;
-    else if (bala == 1) return PARA_ARRIBA;
-    else return PARA_ABAJO;
+    if (bala == 1) return PARA_ARRIBA;
+    return PARA_ABAJO;
 }
 
-dispersion_e buscar_dispersion(const int& bala){
+dispersion_e obtener_dispersion(const int& bala){
     if (bala == 0) return NO;
-    else return ALTA;
+    return ALTA;
 }
 
-bool PewPewLaser::disparar(const orientacion_e& direccion, Mapa& mapa) {
-    if (this->municiones == 0) return false;
-    inclinacion_e inc;
-    dispersion_e dis;
-    for (int i = 0; i < ConfigJuego::LASERS_POR_DISPARO_PP; i++){
-        Municion m(ID_PP_LASER, this->posicion_spawn, (ConfigJuego::ALCANCE_MAXIMO_PP_LASER * TILE_A_METRO), direccion, dis, inc, balas.size());    
+bool PewPewLaser::disparar(const orientacion_e& direccion, Mapa& mapa){
+    if (municiones == 0) return false;
+
+    for (int i = 0; i < ConfigJuego::MUNICIONES_PP_LASER; i++){
+        inclinacion_e inc = obtener_inclinacion(i);
+        dispersion_e dis = obtener_dispersion(i);
+        Municion m(ID_PP_LASER, this->posicion_spawn, (ConfigJuego::ALCANCE_MAXIMO_PP_LASER * TILE_A_METRO), direccion, dis, inc, balas.size());
         if (m.avanzar(mapa)){
             balas.push_back(m);
         }
     }
     municiones -= 1;
     return true;
+
 }
